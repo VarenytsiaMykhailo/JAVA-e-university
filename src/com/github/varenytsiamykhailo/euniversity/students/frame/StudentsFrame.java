ee4c103484ef7942f80380ca8b1ea539f5b1e7e9
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-07-28 13:13:53 +0300

    В StudentsFrame.java вынес часть кода в методы createTopPanel и createBottomPanel

--- src/com/github/varenytsiamykhailo/euniversity/students/frame/StudentsFrame.java
+++ src/com/github/varenytsiamykhailo/euniversity/students/frame/StudentsFrame.java
@@ -20,8 +20,21 @@ public class StudentsFrame extends JFrame {
         // Устанавливаем layout (область) по всему окну
         getContentPane().setLayout(new BorderLayout());
 
+        // Верхняя панель (здесь будет поле для ввода года со спинером)
+        JPanel topPanel = createTopPanel();
+        // Нижняя панель (внутри нее - левая панель (список групп) и правая панель (список студентов))
+        JPanel bottomPanel = createBottomPanel();
 
-        // Верхняя панель (здесь будет поле для ввода года)
+        // Вставляем верхнюю (topPanel) и нижнюю панели (bottomPanel) в форму
+        getContentPane().add(topPanel, BorderLayout.NORTH);
+        getContentPane().add(bottomPanel, BorderLayout.CENTER);
+
+        // Задаем границы формы
+        setBounds(400, 100, 1000,600);
+    }
+
+    // Верхняя панель (здесь будет поле для ввода года со спинером)
+    private JPanel createTopPanel() {
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
@@ -31,20 +44,16 @@ public class StudentsFrame extends JFrame {
         SpinnerModel spinnerModel = new SpinnerNumberModel(2020, 1900, 2100, 1);
         yearSpinner = new JSpinner(spinnerModel);
         topPanel.add(yearSpinner);
+        return topPanel;
+    }
 
-
+    // Нижняя панель (внутри нее - левая панель (список групп) и правая панель (список студентов))
+    private JPanel createBottomPanel() {
         // Нижняя панель
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new BorderLayout()); // все оставшееся место ниже topPanel
+        ms = ManagementSystem.getInstance(); // Коннект к базе происходит внутри ManagementSystem. ManagementSystem сам обработает исключение коннекта
 
-
-        // Левая панель для вывода списка групп
-        JPanel leftPanel = new JPanel();
-        leftPanel.setLayout(new BorderLayout());
-        leftPanel.setBorder(new BevelBorder(BevelBorder.RAISED));
-
-        // Получаем список групп
-        ms = ManagementSystem.getInstance(); // коннект к базе происходит внутри ManagementSystem. ManagementSystem сам обработает исключение коннекта
 
         // Получаем список групп и студентов
         Vector<Group> allGroups = null;
@@ -55,19 +64,22 @@ public class StudentsFrame extends JFrame {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        leftPanel.add(new JLabel("Группы:"), BorderLayout.NORTH);
 
-        allGroupsJList = new JList(allGroups); //создаем визуальный список
-        leftPanel.add(new JScrollPane(allGroupsJList), BorderLayout.CENTER); // вставляем список в скроллируемую панель (JScrollPane), которую помещаем в центре leftPanel
+
+        // Левая панель для вывода списка групп
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.setBorder(new BevelBorder(BevelBorder.RAISED));
+        leftPanel.add(new JLabel("Группы:"), BorderLayout.NORTH);
+        allGroupsJList = new JList(allGroups); // Создаем визуальный список
+        leftPanel.add(new JScrollPane(allGroupsJList), BorderLayout.CENTER); // Вставляем список в скроллируемую панель (JScrollPane), которую помещаем в центре leftPanel
 
 
         // Правая панель для вывода списка студентов
         JPanel rightPanel = new JPanel();
         rightPanel.setLayout(new BorderLayout());
         rightPanel.setBorder(new BevelBorder(BevelBorder.RAISED));
-
         rightPanel.add(new JLabel("Студенты:"), BorderLayout.NORTH);
-
         allStudentsJList = new JList(allStudents);
         rightPanel.add(new JScrollPane(allStudentsJList), BorderLayout.CENTER); // вставляем список в скроллируемую панель (JScrollPane), которую помещаем в центре leftPanel
 
@@ -76,11 +88,6 @@ public class StudentsFrame extends JFrame {
         bottomPanel.add(leftPanel, BorderLayout.WEST);
         bottomPanel.add(rightPanel, BorderLayout.CENTER);
 
-        // Вставляем верхнюю (topPanel) и нижнюю панели (bottomPanel) в форму
-        getContentPane().add(topPanel, BorderLayout.NORTH);
-        getContentPane().add(bottomPanel, BorderLayout.CENTER);
-
-        // Задаем границы формы
-        setBounds(400, 100, 800,600);
+        return bottomPanel;
     }
 }
