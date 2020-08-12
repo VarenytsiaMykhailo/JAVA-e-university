3069b3f4536736d87754bb13f177e254ba89cd83
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-08-12 18:12:44 +0300

    реализовал структуру данных-форму для редактирования/добавления нового студента StudentDataFormForDisplay.java

---
+++ src/com/github/varenytsiamykhailo/euniversity/web/forms/StudentDataFormForDisplay.java
@@ -0,0 +1,119 @@
+package com.github.varenytsiamykhailo.euniversity.web.forms;
+
+import com.github.varenytsiamykhailo.euniversity.logic.Group;
+import com.github.varenytsiamykhailo.euniversity.logic.Student;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class StudentDataFormForDisplay {
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+    private int studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String patronymic;
+
+    private String dateOfBirth;
+
+    private int sex;
+
+    private int groupId;
+
+    private int educationYear;
+
+    private ArrayList<Group> allGroups;
+
+    public void initFromStudent(Student student) {
+        this.studentId = student.getStudentId();
+        this.firstName = student.getFirstName();
+        this.lastName = student.getLastName();
+        this.patronymic = student.getPatronymic();
+        this.dateOfBirth = simpleDateFormat.format(student.getDateOfBirth());
+        if (student.getSex() == 'М') {
+            this.sex = 0; // Мужчина
+        } else if (student.getSex() == 'Ж') {
+            this.sex = 1; // Женщина
+        } else {
+            this.sex = 3; // Неопределенный пол
+        }
+        this.groupId = student.getGroupId();
+        this.educationYear = student.getEducationYear();
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getEducationYear() {
+        return educationYear;
+    }
+
+    public void setEducationYear(int educationYear) {
+        this.educationYear = educationYear;
+    }
+
+    public ArrayList<Group> getAllGroups() {
+        return allGroups;
+    }
+
+    public void setAllGroups(ArrayList<Group> allGroups) {
+        this.allGroups = allGroups;
+    }
+}
