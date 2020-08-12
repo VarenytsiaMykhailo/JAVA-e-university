4ec75b66cc2282a0cd6174dc4e581898ea2e81b5
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-08-12 22:12:10 +0300

    реализовал MainPageServlet.java

---
+++ src/com/github/varenytsiamykhailo/euniversity/web/MainPageServlet.java
@@ -0,0 +1,148 @@
+package com.github.varenytsiamykhailo.euniversity.web;
+
+import com.github.varenytsiamykhailo.euniversity.logic.Group;
+import com.github.varenytsiamykhailo.euniversity.logic.ManagementSystem;
+import com.github.varenytsiamykhailo.euniversity.logic.Student;
+import com.github.varenytsiamykhailo.euniversity.web.forms.MainDataFormForDisplay;
+import com.github.varenytsiamykhailo.euniversity.web.forms.StudentDataFormForDisplay;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+
+public class MainPageServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+        req.setCharacterEncoding("UTF-8"); // Установка кодировки для принятия параметров (запроса)
+
+        if (req.getParameter("insert_student") != null) {
+            // Вызов другой формы, которая перенеаправляет сервлет на другую JSP для ввода данных о новом студенте
+            try {
+                Student student = new Student();
+                student.setStudentId(0);
+                student.setDateOfBirth(new Date());
+                student.setEducationYear(Calendar.getInstance().get(Calendar.YEAR)); // Дефолтное значение - текущий год
+                ArrayList<Group> allGroups = ManagementSystemWeb.getInstance().getAllGroups();
+                StudentDataFormForDisplay studentDataFormForDisplay = new StudentDataFormForDisplay();
+                studentDataFormForDisplay.initFromStudent(student);
+                studentDataFormForDisplay.setAllGroups(allGroups);
+                req.setAttribute("studentDataForm", studentDataFormForDisplay);
+                getServletContext().getRequestDispatcher("/StudentFrame.jsp").forward(req, resp);
+
+                return;
+
+            } catch (SQLException e) {
+                throw new IOException(e.getMessage());
+            }
+        }
+
+        if (req.getParameter("update_student") != null) {
+            // Вызов другой формы, которая перенеаправляет сервлет на другую JSP для ввода данных о студенте
+            try {
+                if (req.getParameter("student_id") != null) {
+                    int studentId = Integer.parseInt(req.getParameter("student_id"));
+                    Student student = ManagementSystemWeb.getInstance().getStudentById(studentId);
+                    ArrayList<Group> allGroups = ManagementSystemWeb.getInstance().getAllGroups();
+                    StudentDataFormForDisplay studentDataFormForDisplay = new StudentDataFormForDisplay();
+                    studentDataFormForDisplay.initFromStudent(student);
+                    studentDataFormForDisplay.setAllGroups(allGroups);
+                    req.setAttribute("studentDataForm", studentDataFormForDisplay);
+                    getServletContext().getRequestDispatcher("/StudentFrame.jsp").forward(req, resp);
+
+                    return;
+
+                }
+            } catch (SQLException e) {
+                throw new IOException(e.getMessage());
+            }
+        }
+
+        if (req.getParameter("delete_student") != null) {
+            try {
+                if (req.getParameter("student_id") != null) {
+                    Student student = new Student();
+                    student.setStudentId(Integer.parseInt(req.getParameter("studentId")));
+                    ManagementSystemWeb.getInstance().deleteStudent(student);
+                }
+            } catch (SQLException e) {
+                throw new IOException(e.getMessage());
+            }
+        }
+
+        String groupIdString = req.getParameter("group_id");
+        String yearString = req.getParameter("year");
+
+        if (req.getParameter("move_group") != null) {
+            // Перемещаем всех студентов в другую группу
+            String newGroupIdString = req.getParameter("new_group_id");
+            String newYearString = req.getParameter("new_year");
+            try {
+                Group group = new Group();
+                group.setGroupId(Integer.parseInt(groupIdString));
+
+                Group newGroup = new Group();
+                newGroup.setGroupId(Integer.parseInt(newGroupIdString));
+
+                ManagementSystemWeb.getInstance().moveStudentsFromGroupToNewGroup(group, Integer.parseInt(yearString), newGroup, Integer.parseInt(newYearString));
+
+                // Устанавливаем новые значения. Нужно для показа группы, в которую мы переместили студентов
+                groupIdString = newGroupIdString;
+                yearString = newYearString;
+            } catch (SQLException e) {
+                throw new IOException(e.getMessage());
+            }
+        }
+
+        int selectedGroupId = -1;
+        if (groupIdString != null) {
+            selectedGroupId = Integer.parseInt(groupIdString);
+        }
+
+        int selectedYear = Calendar.getInstance().get(Calendar.YEAR); // Дефолтное значение - текущий год
+        if (yearString != null) {
+            selectedYear = Integer.parseInt(yearString);
+        }
+
+        MainDataFormForDisplay mainDataFormForDisplay = new MainDataFormForDisplay();
+        try {
+            ArrayList<Group> allGroups = ManagementSystemWeb.getInstance().getAllGroups();
+            Group group;
+            if (selectedGroupId == -1) {
+                Iterator it = allGroups.iterator();
+                group = (Group) it.next();
+            } else { // Если новая группа уже выбрана в списке (после перевода студентов в новую группу)
+                group = new Group();
+                group.setGroupId(selectedGroupId);
+            }
+
+            ArrayList<Student> studentsForSelectedGroup = ManagementSystemWeb.getInstance().getStudentsFromGroup(group, selectedYear);
+            mainDataFormForDisplay.setSelectedGroupId(selectedGroupId);
+            mainDataFormForDisplay.setSelectedYear(selectedYear);
+            mainDataFormForDisplay.setAllGroups(allGroups);
+            mainDataFormForDisplay.setStudentsForSelectedGroup(studentsForSelectedGroup);
+        } catch (SQLException e) {
+            throw new IOException(e.getMessage());
+        }
+
+        req.setAttribute("mainDataForm", mainDataFormForDisplay);
+        getServletContext().getRequestDispatcher("/MainPageFrame.jsp").forward(req, resp);
+    }
+}
