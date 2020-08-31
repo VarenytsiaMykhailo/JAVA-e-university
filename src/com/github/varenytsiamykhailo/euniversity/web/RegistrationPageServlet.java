c4f0bce67c8de8fd9a88703f4db115b2907adf31
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-08-31 12:02:28 +0300

    реализовал серверный валидатор вводимых данных в RegistrationPageServlet.java, на всякий случай, если пользователь обойдет js-валидатор

--- src/com/github/varenytsiamykhailo/euniversity/web/RegistrationPageServlet.java
+++ src/com/github/varenytsiamykhailo/euniversity/web/RegistrationPageServlet.java
@@ -3,6 +3,9 @@ package com.github.varenytsiamykhailo.euniversity.web;
 import com.github.varenytsiamykhailo.euniversity.logic.Role;
 import com.github.varenytsiamykhailo.euniversity.logic.User;
 
+import com.github.varenytsiamykhailo.euniversity.web.Exceptions.IllegalRequestException;
+import com.github.varenytsiamykhailo.euniversity.web.Exceptions.validatorExceptions.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +15,6 @@ import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class RegistrationPageServlet extends HttpServlet {
-
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
-    }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -35,8 +33,6 @@ public class RegistrationPageServlet extends HttpServlet {
             throw new IOException(new IllegalRequestException());
         }
 
-        @SuppressWarnings("unchecked") final AtomicReference<ManagementSystemWebDAO> managementSystemWebDAO = (AtomicReference<ManagementSystemWebDAO>) req.getServletContext().getAttribute("managementSystemWebDAO");
-
         final String login = req.getParameter("login");
         final String loginRepeat = req.getParameter("login_repeat");
         final String email = req.getParameter("email");
@@ -45,6 +41,7 @@ public class RegistrationPageServlet extends HttpServlet {
         final String passwordRepeat = req.getParameter("password_repeat");
         final String role = req.getParameter("role");
 
+        /* Отладка
         System.out.println(login);
         System.out.println(loginRepeat);
         System.out.println(email);
@@ -52,17 +49,12 @@ public class RegistrationPageServlet extends HttpServlet {
         System.out.println(password);
         System.out.println(passwordRepeat);
         System.out.println(role);
+        */
 
-        // Проверки на равенство данных (на случай, если что-то случилось с js скриптом)
-        if (!login.equals(loginRepeat)) {
-            throw new IOException(new LoginsDontEqualsException());
-        }
-        if (!email.equals(emailRepeat)) {
-            throw new IOException(new EmailsDontEqualsException());
-        }
-        if (!password.equals(passwordRepeat)) {
-            throw new IOException(new PasswordsDontEqualsException());
-        }
+        // Валидатор введенных данных (на случай, если что-то случилось с js скриптом)
+        validator(login, loginRepeat, email, emailRepeat, password, passwordRepeat);
+
+        @SuppressWarnings("unchecked") final AtomicReference<ManagementSystemWebDAO> managementSystemWebDAO = (AtomicReference<ManagementSystemWebDAO>) req.getServletContext().getAttribute("managementSystemWebDAO");
 
         try {
             if (managementSystemWebDAO.get().userIsExistByLogin(login)) {
@@ -103,28 +95,36 @@ public class RegistrationPageServlet extends HttpServlet {
         getServletContext().getRequestDispatcher("/RegistrationPage.jsp").forward(req, resp);
 
     }
-}
 
-class LoginsDontEqualsException extends Exception {
-    public LoginsDontEqualsException() {
-        super("Login is not equal login repeat.");
-    }
-}
+    void validator(String login, String loginRepeat, String email, String emailRepeat, String password, String passwordRepeat) throws IOException {
+        // Регулярные выражения для валидации
+        String regExpLogin = "^[a-zA-Z][a-zA-Z0-9-_]{3,21}$"; // минимум 4 символа, максимум 23; допускаются строчные и заглавные латинские буквы и цифры; первый символ обязательно буква
+        /*
+         * Регулярка соответствует емаилу из стандарта RFC 5322
+         * https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression/201378#201378
+         * */
+        String regExpEmail = "^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$"; // минимум 2 символа после "@"; минимум 2 символа после ".";
+        String regExpPassword = "^[0-9a-zA-Z]{6,}$"; // минимум 6 символов; допускаются строчные и заглавные латинские буквы и цифры;
 
-class EmailsDontEqualsException extends Exception {
-    public EmailsDontEqualsException() {
-        super("Email is not equal email repeat.");
-    }
-}
+        // Выбрасываем исключения, если введены неверные данные
+        if (!login.matches(regExpLogin)) {
+            throw new IOException(new IncorrectLoginInputException());
+        }
+        if (!email.matches(regExpEmail)) {
+            throw new IOException(new IncorrectEmailInputException());
+        }
+        if (!password.matches(regExpPassword)) {
+            throw new IOException(new IncorrectPasswordInputException());
+        }
 
-class PasswordsDontEqualsException extends Exception {
-    public PasswordsDontEqualsException() {
-        super("Password is not equal password repeat.");
-    }
-}
-
-class IllegalRequestException extends Exception {
-    public IllegalRequestException() {
-        super("Illegal request exception.");
+        if (!login.equals(loginRepeat)) {
+            throw new IOException(new LoginsDontEqualsException());
+        }
+        if (!email.equals(emailRepeat)) {
+            throw new IOException(new EmailsDontEqualsException());
+        }
+        if (!password.equals(passwordRepeat)) {
+            throw new IOException(new PasswordsDontEqualsException());
+        }
     }
 }
